f6a78686987287ac1f4d1798aa898ec8bd76e373
HW <a.k_windsor@protonmail.com>
2022-01-11 10:03:15 +0100

    added space

--- fotogallerie/js/script.js
+++ fotogallerie/js/script.js
@@ -3,7 +3,8 @@ let images = [
   './img/img5.jpg', './img/img6.jpg', './img/img7.jpg', './img/img8.jpg',
   './img/img9.jpg', './img/img10.jpg', './img/img11.jpg', './img/img12.jpg',
   './img/img13.jpg', './img/img14.jpg', './img/img15.jpg', './img/img16.jpg', 
-  './img/img17.jpg', './img/img18.jpg', './img/img19.jpg', './img/img20.jpg', './img/img21.jpg'];
+  './img/img17.jpg', './img/img18.jpg', './img/img19.jpg', './img/img20.jpg', 
+  './img/img21.jpg'];
 
 let imgContainer = document.getElementById('image-container');
 
