bcd8a26d9beb1f25843e373af517a465dc31e063
HW <a.k_windsor@protonmail.com>
2022-01-30 09:59:36 +0100

    small changes

--- fotogallerie/js/script.js
+++ fotogallerie/js/script.js
@@ -6,92 +6,59 @@ let images = [
   './img/img17.jpg', './img/img18.jpg', './img/img19.jpg', './img/img20.jpg',
   './img/img21.jpg'
 ];
-
-function $(id) {
-  document.getElementById(id);
-}
-
-let imgContainer = $('imgContainer');
-let bigScreen = $('bigScreen');
-let symbols = $('bigScreenSymbols');
-
+let header = document.getElementById('header');
+let imgContainer = document.getElementById('image-container');
+let bigScreen = document.getElementById('bigScreen');
+let symbols = document.getElementById('bigScreenSymbols')
 
 function renderImages() {
+
   for (let i = 0; i < images.length; i++) {
     imgContainer.innerHTML += `
         <div class="img-box">
-          <img onclick="openImg()" src="${images[i]}">
+          <img onclick="openImg(${i})" src="${images[i]}">
         </div>
       `;
   }
 };
 
-function openImg() {
-  imgContainer.classList.add('d-none');
-  alert('You touched picture soundso');
+function openImg(i) {
+  bigPic()
+  bigScreen.innerHTML += `
+  <img src="${images[i]}">
+  <div id="bigScreenSymbols" class="bigScreen-symbols">
+  <i class="fas fa-times exit" onclick="closeImg()"></i>
+  <div id="left-right">
+    <i class="fas fa-chevron-circle-left" onclick="previousImg(${i})"></i>
+    <i class="fas fa-chevron-circle-right" onclick="nextImg(${i})"></i>
+  </div>
+  `;
+
 }
 
-function closeImg() {}
+function bigPic() {
+  header.classList.add('d-none');
+  bigScreen.classList.remove('d-none');
+  imgContainer.style = 'display: none';
+  bigScreen.style = 'display: flex;'
+  bigScreen.innerHTML = ''
 
-function previousImg() {}
+}
 
-function nextImg() {}
-// let column1 = document.getElementsByClassName('photos_column')[0];
-// let column2 = document.getElementsByClassName('photos_column')[1];
-// let column3 = document.getElementsByClassName('photos_column')[2];
-// let x = window.matchMedia('(max-width: 900px)');
+function closeImg() {
+  header.classList.remove('d-none');
+  bigScreen.style = 'display: none'
+  imgContainer.style = 'display: '
+}
 
-// function renderImages(){
+function previousImg(i) {
+  i--
+  if (i >= 0) openImg(i)
+  else openImg(images.length - 1)
+}
 
-//   columnQuery(x)
-// } 
-
-// function columnQuery(x){
-//   if(x.matches){
-//     column3.classList.add('d-none');
-//     for(let i=0; i < images.length; i++) {
-
-//       column1.innerHTML += `
-//       <div class="img-box">
-//         <img src="${images[i]}">
-//       </div>`;
-//       i++;
-//     }
-//     for(let i=1; i < images.length; i++){
-//       column2.innerHTML += `
-//       <div class="img-box">
-//         <img src="${images[i]}">
-//       </div>`;
-//       i++;
-//   }
-// }else{
-//   column3.classList.remove('d-none');
-
-//   for(let i=0; i < images.length; i++) {
-
-//     column1.innerHTML += `
-//     <div class="img-box">
-//       <img src="${images[i]}">
-//     </div>`;
-//     i = i + 2;
-
-//   }
-
-//   for(let i=1; i < images.length; i++){
-//     column2.innerHTML += `
-//     <div class="img-box">
-//       <img src="${images[i]}">
-//     </div>`;
-//     i = i + 2;
-//   }
-
-//   for(let i=2; i < images.length; i++){
-//     column3.innerHTML += `
-//     <div class="img-box">
-//       <img src="${images[i]}">
-//     </div>`;
-//     i = i + 2;
-//   }
-
-// }
-// };
+function nextImg(i) {
+  i++
+  if (i < images.length) openImg(i)
+  else openImg(0)
+}
